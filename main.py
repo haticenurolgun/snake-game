2b24a26525069e52c031ec4e557130bb53c0a85e
Hatice Olgun <hatiicenurolgun@gmail.com>
2025-09-06 14:08:24 +0300

    Added food class and growth mechanics
    - Implemented Food class with random positioning
    - Added snake growth mechanism when eating food
    - Implemented collision detection with own tail
    - Game over condition added for self-collision
    - Prepared infrastructure for score system

--- main.py
+++ main.py
@@ -7,17 +7,21 @@
 #todo 7:detect collision with snakes body
 
 import time
-from turtle import Turtle,Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
+
 
 screen=Screen()
 screen.setup(600,600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
-
+from scoreboard import ScoreBoard
 
 snake=Snake()
+food=Food()
+scoreboard=ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -31,6 +35,28 @@ while is_game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    #detect collision with food
+    if snake.head.distance(food)<15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #collision with wall
+    if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
+        is_game_on= False
+        scoreboard.game_over()
+
+    for segment in snake.segments:
+
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment)<10:
+            is_game_on= False
+            scoreboard.game_over()
+
+
+
 
 
 
