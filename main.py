5fe0e75301b89d4910e72296b19fcb6f0b4ec7d3
Hatice Olgun <hatiicenurolgun@gmail.com>
2025-09-10 10:48:17 +0300

    Added high score feature

--- main.py
+++ main.py
@@ -44,19 +44,16 @@ while is_game_on:
 
     #collision with wall
     if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
-        is_game_on= False
-        scoreboard.game_over()
+        scoreboard.reset_score()
+        snake.reset()
 
     for segment in snake.segments:
 
         if segment == snake.head:
             pass
         elif snake.head.distance(segment)<10:
-            is_game_on= False
-            scoreboard.game_over()
-
-
-
+            scoreboard.reset_score()
+            snake.reset()
 
 
 
--- scoreboard.py
+++ scoreboard.py
@@ -1,9 +1,15 @@
+
 from turtle import Turtle
 
 class ScoreBoard(Turtle):
+
     def __init__(self):
         super().__init__()
         self.score=0
+        with open("data.txt") as data:
+
+            self.high_score= int(data.read())
+
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -11,14 +17,23 @@ class ScoreBoard(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score:{self.score}", align="center", font=("Courier", 18, "normal"))
+        self.clear()
+        self.write(f"Score:{self.score}  High score:{self.high_score}" ,align="center", font=("Courier", 18, "normal"))
+
+
+    def reset_score(self):
+        if self.score>self.high_score:
+            self.high_score=self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+
+        self.score=0
+        self.update_score()
+
 
     def increase_score(self):
         self.score +=1
-        self.clear()
+
         self.update_score()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game Over",align="center",font =("Courier", 18, "normal"))
 
